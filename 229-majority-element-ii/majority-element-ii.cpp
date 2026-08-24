2c89d62b43b0d8967282fd32ac7db6fa121832ee
amanpandey05 <7266064524aman@gmail.com>
2026-08-24 19:51:36 +0530

    Time: 7 ms (40.05%) | Memory: 26.7 MB (9.88%) - LeetSync

---
+++ 229-majority-element-ii/majority-element-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+       unordered_map<int, int> mp;
+       vector<int> ans;
+       for(auto x: nums) {
+        mp[x]++;
+       }
+       for(auto i: mp) {
+        if(i.second > nums.size()/3) {
+            ans.push_back(i.first);
+        }
+       }
+       return ans;
+    }
+};
