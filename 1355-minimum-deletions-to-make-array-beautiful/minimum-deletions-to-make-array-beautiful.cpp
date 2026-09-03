026ee69bd912ccaefb436bbb1189d26686990051
amanpandey05 <7266064524aman@gmail.com>
2026-09-03 17:07:28 +0530

    Time: 13 ms (12.57%) | Memory: 129.2 MB (21.81%) - LeetSync

--- 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.cpp
+++ 1355-minimum-deletions-to-make-array-beautiful/minimum-deletions-to-make-array-beautiful.cpp
@@ -4,29 +4,34 @@ public:
         int n = nums.size();
         if(n == 1) return 1;
         stack<int> st;
-        int cnt = 0;
-        for(int i = 0; i<n-1; i++) {
+        for(int i = 0; i<n; i++) {
            
-        //     if(st.size()%2 == 0) {
-        //         st.push(nums[i]);
-        //     } 
-        //     else {
-               
-        //    }
-        // }
-        // if(st.size() % 2 != 0) {
-        //     return abs(n - (int)st.size() - 1);
-        // } else {
-        //     return abs(n - (int)st.size());
-            if((i-cnt) % 2 == 0 && nums[i] == nums[i+1]) {
-                cnt++;
-            } else {
-                continue;
+            if(st.size()%2 == 0) {
+                st.push(nums[i]);
+            } 
+            else {
+               if(st.top() != nums[i]) {
+                st.push(nums[i]);
+               }
             }
         } 
-        if((n-cnt) % 2 != 0) cnt++;
+        int cnt = n - st.size();
+
+        if(st.size() % 2 != 0) {
+            cnt++;
+        }
+
         return cnt;
+  
     }
     
 };
+// if((i-cnt) % 2 == 0 && nums[i] == nums[i+1]) {
+//                 cnt++;
+//             } else {
+//                 continue;
+//             }
+//         } 
+//         if((n-cnt) % 2 != 0) cnt++;
+//         return cnt;
 
